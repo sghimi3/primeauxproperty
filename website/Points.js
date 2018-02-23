36a8af60c9a24d3a805385a726680b6af743632a
rieou Draco <33807683+rieou@users.noreply.github.com>
2018-02-23 11:50:23 -0600

    Update Points.js

--- website/Points.js
+++ website/Points.js
@@ -1,7 +1,25 @@
-var map;
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-	center: {lat: -34.397, lng: 150.644},
-	zoom: 8
-  });
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 8,
+      center: {lat: -34.397, lng: 150.644}
+    });
+    var geocoder = new google.maps.Geocoder();
+    GeocodeSet(geocoder, map);
+}
+function GeocodeSet(geocoder, resultsMap) {
+    var address = "3126 Caroljack Dr.";
+    geocoder.geocode({'address': address}, function(results, status) {
+      if (status === 'OK') {
+        resultsMap.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+          map: resultsMap,
+          position: results[0].geometry.location
+        });
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+function GeocodeConvert(geocoder, address)
+{
+
 }
