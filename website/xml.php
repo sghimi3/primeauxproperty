ead989d5519acb29611b672ab1cb7064ac3112dc
Josh <jam-millsap@users.noreply.github.com>
2018-02-23 23:56:07 -0600

    It's done.

---
+++ website/xml.php
@@ -0,0 +1,14 @@
+<?php
+
+$url="http://www.zillow.com/webservice/GetRegionChildren.htm?zws-id=X1-ZWz18r2dv9h4az_9prl8&state=la&city=batonrouge&childtype=neighborhood";
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_URL, $url);    // get the url contents
+
+$data = curl_exec($ch); // execute curl request
+curl_close($ch);
+
+$xml = simplexml_load_string($data);
+header("Content-type: text/xml; charset=utf-8");
+print_r($xml->asXML());
+?>
