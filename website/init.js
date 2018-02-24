9217c3a469214332c89e2548145dd67e8cacd0f2
Sweta Ghimire <sghimi3@lsu.edu>
2018-02-23 18:18:45 -0600

    Attempt to fix crime loading problem

--- website/init.js
+++ website/init.js
@@ -1,9 +1,6 @@
 var markers  = [];
 var lat;
 var long;
-var locations = [];
-var schools = [];
-
 
 
 function initAutocomplete() {
@@ -151,36 +148,3 @@ function codeAddress(Address, geocoder, map) {
   });
 }
 
-var call1 = $.ajax({
-    url: "https://data.brla.gov/resource/5rji-ddnu.json",
-    type: "GET",
-    data: {
-      "$limit" : 1000,
-      "$$app_token" : "OBimKBKVR5rBKKLHnMg5ENdx2"
-    }
-}).done(function(data) {
-    for (var i = 0; i < data.length; i++) {
-        if (data[i].geolocation != null) {
-          var coords = data[i].geolocation.coordinates;
-          var test = {lat: coords[1], lng: coords[0]};
-          locations.push(test);
-        }
-    }
-});
-
-
-var call2 = $.ajax({
-    url: "https://data.brla.gov/resource/4gku-4cqw.json",
-    type: "GET",
-    data: {
-      "$$app_token" : "OBimKBKVR5rBKKLHnMg5ENdx2"
-    }
-}).done(function(data) {
-    for (var i = 0; i < data.length; i++) {
-        if (data[i].geolocation != null) {
-          var coords = data[i].geolocation.coordinates;
-          var test = {lat: coords[1], lng: coords[0]};
-          schools.push(test);
-        }
-    }
-});
